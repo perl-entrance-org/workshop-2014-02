71467b18ec0d961f3d6b47578f40218d583b2165
papix <mail@papix.net>
2014-06-24 13:17:09 +0900

    文章を｢ですます｣に統一, その他細かい修正

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+cat intro.md > slide.md
+echo -e "\n" >> slide.md
+
+cat use.md > slide.md
+echo -e "\n" >> slide.md
+
+cat scalar.md > slide.md
+echo -e "\n" >> slide.md
+
+cat stdin-calc.md > slide.md
+echo -e "\n" >> slide.md
+
+cat if-else.md > slide.md
+echo -e "\n" >> slide.md
+
+cat logical-operator.md > slide.md
+echo -e "\n" >> slide.md
+
+cat array-for.md > slide.md
+echo -e "\n" >> slide.md
+
+cat outro.md > slide.md
+echo -e "\n" >> slide.md
