46f83aade74f39feac91293bcc049b32325bbb8e
papix <mail@papix.net>
2014-06-28 13:19:57 +0900

    revealup化

---
+++ build.pl
@@ -0,0 +1,14 @@
+use strict;
+use warnings;
+use utf8;
+
+use Path::Tiny;
+
+my @slides = qw/ intro.md use.md scalar.md stdin-calc.md if-else.md logical-operator.md array-for.md outro.md /;
+
+for my $file (@slides) {
+    my $str = path($file)->slurp_utf8;
+    $str =~ s/\n#/---\n#/g;
+    print $str;
+}
+
